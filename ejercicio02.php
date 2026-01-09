7cf4be7fb6a018dca768962355eab33d21413c80
Juan Iglesias <iglesias.juan@outlook.es>
2026-01-09 18:57:55 +0100

    Feat: commit 4
    Inicio del ejercicio02

---
+++ ejercicio02.php
@@ -0,0 +1,24 @@
+<?php
+
+// 1. Array del carrito.
+
+$carrito = [
+  ["producto" => "Teléfono", "precio" => 1499.99, "cantidad" => 1],
+  ["producto" => "Auriculares", "precio" => 605.99, "cantidad" => 1],
+  ["producto" => "Ratón", "precio" => 29.99, "cantidad" => 3],
+  ["producto" => "Teclado", "precio" => 94.99, "cantidad" => 4],
+  ["producto" => "Pantalla", "precio" => 652.95, "cantidad" => 1],
+  ["producto" => "Portátil", "precio" => 2250.90, "cantidad" => 7],
+];
+
+// 2. Productos del carrito (nombre, precio unitario, cantidad y subtotal).
+
+foreach ($carrito as $articulo) {
+  // Calcula el subtotal de cada articulo
+  $subtotal = $articulo["precio"] * $articulo["cantidad"];
+  // Información
+  echo "Nombre: " . $articulo["producto"] . "<br>";
+  echo "Precio unitario: " . number_format($articulo["precio"], 2) . " €" . "<br>";
+  echo "Cantidad: " . $articulo["cantidad"] . "<br>";
+  echo "Subtotal: " . number_format($subtotal, 2) . " €" . "<br>";
+}

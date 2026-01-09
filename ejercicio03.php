aaf9b416f6ef9eccaded16bf37c553cf3251a23b
Juan Iglesias <iglesias.juan@outlook.es>
2026-01-09 20:37:31 +0100

    Feat: commit 9

--- ejercicio03.php
+++ ejercicio03.php
@@ -1,2 +1,22 @@
 <?php
 
+// Texto para analizar.
+$texto = "Era un día frío y luminoso de abril y los relojes estaban dando las trece. Winston Smith, con la barbilla clavada en el pecho en un esfuerzo por escapar al desagradable viento, pasó a toda prisa entre las puertas de cristal de las Casas de la Victoria, aunque no lo bastante rápido para impedir que se colara tras él un remolino de polvo y suciedad";
+// 1. Convertir el texto a minúsculas.
+$texto = strtolower($texto);
+
+// 2. Contar cuántas palabras tiene el texto.
+// Separar en palabras.
+$palabras = explode(" ", $texto);
+// Ignorar palabras de menos de 3 letras.
+$palabrasIgnoradas = [];
+
+foreach ($palabras as $palabra) {
+  if (strlen($palabra) >= 3) {
+    $palabrasIgnoradas[] = $palabra;
+  }
+}
+
+$totalPalabras = count($palabrasIgnoradas)
+
+// 3. Contar cuántas veces aparece cada palabra.
